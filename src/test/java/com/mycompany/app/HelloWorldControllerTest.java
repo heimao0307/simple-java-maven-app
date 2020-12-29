b038885a880f8520fa7a6d1778a6f6336dcf4dba
heimao <fengyun86868@163.com>
2020-12-29 09:17:41 +0800

    Create HelloWorldControllerTest.java

---
+++ src/test/java/com/mycompany/app/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldControllerTest {
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Really appreciate your star, that's the power of our life.", new HelloWorldController().sayHello());
+    }
+}
